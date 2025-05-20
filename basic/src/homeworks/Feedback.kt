f0077f2fa4765de82ba49dc1fbccec62a2922da2
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-20 09:47:31 +0200

    Creado archivo Feedback.kt con su logica de tarea realizada

---
+++ basic/src/homeworks/Feedback.kt
@@ -0,0 +1,14 @@
+package homeworks
+
+fun main(){
+
+    val rankingInput = "Input your value from 1 to 5"
+    println(rankingInput)
+    val ranking = readln()
+
+    val opinionInput = "Input your opinion"
+    println(opinionInput)
+    val opinion = readln()
+
+    println("Thank you for your feedback! \nYour value is: $ranking \nYour opinion: $opinion")
+}
