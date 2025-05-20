dfd82ff3b71f3c68551b3808547d21b3128cc188
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-20 10:53:06 +0200

    Cambios en archivo Conditional.kt añadidos

--- basic/src/Conditional.kt
+++ basic/src/Conditional.kt
@@ -1,10 +1,18 @@
 fun main() {
     val temperature = readln().toInt()
-    if(temperature >= 25){
+
+    val hot = temperature > 25 // true
+    val cold = temperature < 20 // true
+    //if(temperature >= 25){
+    if(hot){
         println("The AC in On")
-    }else if (temperature <= 20){
+    //}else if (temperature <= 20){
+    }else if (cold){
         println("The AC in Off")
     }else{
         println("The AC is idle")
     }
+
+
+
 }
