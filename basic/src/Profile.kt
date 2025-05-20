d2071b0f70e2a5a0369aeb0a23a9fbfcf2d369ea
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-20 09:28:21 +0200

    Creado archivo Profile.kt con su logica

---
+++ basic/src/Profile.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+    val inputName = "Input your name"
+    println(inputName)
+    val name = readln()
+    val inputAge = "Input your age"
+    println(inputAge)
+    val age = readln()
+    val inputSex = "Input your sex"
+    println(inputSex)
+    val sex = readln()
+    val inputHeight = "Input your height"
+    println(inputHeight)
+    val height = readln()
+    val inputWeight = "Input your weight"
+    println(inputWeight)
+    val weight = readln()
+    println("Name: " + name + "\nAge: " + age +"\nSex: " + sex+"\nHeight: " + height+"\nWeight: "+weight)
+
+
+
+}
