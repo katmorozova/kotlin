3ed9c118d86264fce12f7db8a736bc7cfc9130d2
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-22 09:29:01 +0200

    Creado archivo con su logica

---
+++ basic/src/LongNumbers.kt
@@ -0,0 +1,30 @@
+fun main() {
+
+
+    val numbers = 0 .. 1_000_000_000L
+    //var sum: Long = 0
+    var sum = 0L
+
+    for (number in numbers){
+        sum += number
+    }
+    println(sum)
+    //println(numbers.sum())
+
+    var a: Byte = 127
+    a ++
+    print(a)
+
+    var b: Short = 127
+    b ++
+    println(b)
+
+    var c: Int = 127
+    c ++
+    println(c)
+
+    var d: Long = 1_333_333_333
+    d++
+    println(d)
+
+}
