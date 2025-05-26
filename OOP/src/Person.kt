af800536454e3493aa868e166f7c45e7f09017ab
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-26 08:55:24 +0200

    Cambios en archivos Person.kt y Profile.kt añadidos

--- OOP/src/Person.kt
+++ OOP/src/Person.kt
@@ -5,5 +5,15 @@ class Person {
     var height: Int = 0
     var weight: Int = 0
 
+    fun sayHello(){
+        println("Hello!")
+    }
+
+    fun run(){
+        repeat(10){
+            println("Estoy corriendo")
+        }
+    }
+
 
 }
--- OOP/src/Profile.kt
+++ OOP/src/Profile.kt
@@ -1,32 +1,39 @@
 fun main() {
     val first: Person = Person()
     val second: Person = Person()
-    print("Input 1st name")
+    println("Input 1st name")
     first.name = readln()
 
-    print("Input 2nd name")
+    println("Input 2nd name")
     second.name = readln()
 
-    print("Input 1st age")
+    println("Input 1st age")
     first.age = readln().toInt()
 
-    print("Input 2nd age")
+    println("Input 2nd age")
     second.age = readln().toInt()
 
-    print("Input 1st height")
+    println("Input 1st height")
     first.height = readln().toInt()
 
-    print("Input 2nd height")
+    println("Input 2nd height")
     second.height = readln().toInt()
 
-    print("Input 1st weight")
+    println("Input 1st weight")
     first.weight = readln().toInt()
 
-    print("Input 2nd weight")
+    println("Input 2nd weight")
     second.weight = readln().toInt()
 
     //print("Name: $name\nAge: $age")
     print("Name: ${first.name}\nAge: ${first.age} \nAlture: ${first.height} \nWeight: ${first.weight}")
     print("Name: ${second.name}\nAge: ${second.age} \nAlture: ${second.height} \nWeight: ${second.weight}")
+
+
+    first.sayHello()
+    second.sayHello()
+    first.run()
+
+
 
 }
