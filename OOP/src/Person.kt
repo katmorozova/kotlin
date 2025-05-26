11a6f39791ad4e61781a8c5ba84d69a883ff94c1
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-26 08:44:42 +0200

    Creados archivos Person.kt y Profile.kt con su logica

--- OOP/src/Person.kt
+++ OOP/src/Person.kt
@@ -2,6 +2,8 @@ class Person {
 
     var name: String = ""
     var age: Int = 0
+    var height: Int = 0
+    var weight: Int = 0
 
 
 }
--- OOP/src/Profile.kt
+++ OOP/src/Profile.kt
@@ -13,8 +13,20 @@ fun main() {
     print("Input 2nd age")
     second.age = readln().toInt()
 
+    print("Input 1st height")
+    first.height = readln().toInt()
+
+    print("Input 2nd height")
+    second.height = readln().toInt()
+
+    print("Input 1st weight")
+    first.weight = readln().toInt()
+
+    print("Input 2nd weight")
+    second.weight = readln().toInt()
+
     //print("Name: $name\nAge: $age")
-    print("Name: ${first.name}\nAge: ${first.age}")
-    print("Name: ${second.name}\nAge: ${second.age}")
+    print("Name: ${first.name}\nAge: ${first.age} \nAlture: ${first.height} \nWeight: ${first.weight}")
+    print("Name: ${second.name}\nAge: ${second.age} \nAlture: ${second.height} \nWeight: ${second.weight}")
 
 }
