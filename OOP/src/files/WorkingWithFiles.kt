23b727f3ed0b9f485ebd9c3c42b8c1b1ec2d10a5
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-29 09:29:29 +0200

    Creado archivo test.txt y añadidos cambios relacionados en WorkingWithFiles

--- OOP/src/files/WorkingWithFiles.kt
+++ OOP/src/files/WorkingWithFiles.kt
@@ -1,5 +1,10 @@
 package files
 
-fun main() {
+import java.io.File
 
+fun main() {
+//Creamor ejemplar de clase File:
+    val file = File("test.txt")
+    file.writeText("Hello!")//añadir texto-> reemplaza texto
+    file.appendText("World!!")//añade texto -> no elemina texto
 }
