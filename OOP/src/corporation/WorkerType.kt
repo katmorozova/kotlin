1379e02eecfac8a6f21108b0892d791fa00aa9ed
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-05 10:23:17 +0200

    Creado enum clase WorkerType con su logica

---
+++ OOP/src/corporation/WorkerType.kt
@@ -0,0 +1,9 @@
+package corporation
+
+enum class WorkerType (val title: String) {
+
+    DIRECTOR("Director"),
+    ACCOUNTANT("Accountant"),
+    ASSISTANT("Assistant"),
+    CONSULTANT("Consultant")
+}
