5af3eb50e2143265584375d1fab921cfc8f21374
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-05 09:57:34 +0200

    Añadidos tipos de operaciones en OperationType

--- OOP/src/corporation/OperationType.kt
+++ OOP/src/corporation/OperationType.kt
@@ -5,5 +5,8 @@ enum class OperationType (val title: String){
     EXIT("Exit"),
     REGISTER("Register new item"),
     SHOW_ALL_ITEMS("Show all items"),
-    REMOVE_PRODUCT_CARD("Remove product card")
+    REMOVE_PRODUCT_CARD("Remove product card"),
+    REGISTER_NEW_EMPLOYEE("Register new employee"),
+    FIRE_EMPLOYEE("Fire an employee"),
+    SHOW_ALL_EMPLOYEES("Show all employees")
 }
