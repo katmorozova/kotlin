f07231ce6427a74c78ccca2b31d384de8f6701d5
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-10 09:49:06 +0200

    Añadido metodo clean() para Accountant

--- OOP/src/corporation/Accountant.kt
+++ OOP/src/corporation/Accountant.kt
@@ -11,6 +11,10 @@ class Accountant(id: Int, name: String, age: Int): Worker(id, name, age, WorkerT
     private val file = File("product_cards.txt")
     private val workersList = File("workers_cards.txt")
 
+    override fun clean() {
+        println("My position is Accountant. I'm cleaning workplace...")
+    }
+
     override fun work() {
         val operationTypes = OperationType.entries
         while(true){
