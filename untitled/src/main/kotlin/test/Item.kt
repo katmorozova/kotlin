0845463ccd91808e3746ab6403db346ac9309dfb
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-02 15:23:32 +0200

    Creados archivos Test,Item con su logica

---
+++ untitled/src/main/kotlin/test/Item.kt
@@ -0,0 +1,6 @@
+package org.example.test
+
+data class Item(
+    val id: Int,
+    val name: String
+)
---
+++ untitled/src/main/kotlin/test/Test.kt
@@ -0,0 +1,46 @@
+package org.example.test
+
+import java.io.File
+
+fun main() {
+    val file = File("items.txt")
+    writeToFile(file)
+    val items = readFromFile(file)
+    for(item in items){
+        println(item)
+    }
+
+
+}
+
+fun readFromFile(file: File): List<Item> {
+    val items = mutableListOf<Item>()
+    val content = file.readText().trim()
+    val itemsAsString = content.split("\n")
+    for (itemAsString in itemsAsString){
+        val properties = itemAsString.split("%")
+        val id = properties[0].toInt()
+        val name = properties[1]
+        val item = Item(id, name)
+        items.add(item)
+    }
+    return items
+}
+
+fun writeToFile(file: File){
+    val items = mutableListOf<Item>()
+    while (true){
+        println("Enter id or 0 to exit: ")
+        val id = readln().toInt()
+        if (id == 0) break
+        println("Enter name: ")
+        val name = readln()
+        val item = Item(id, name)
+        items.add(item)
+    }
+
+    for (item in items){
+        //println(item)
+        file.appendText("${item.id}%${item.name}\n")
+    }
+}
