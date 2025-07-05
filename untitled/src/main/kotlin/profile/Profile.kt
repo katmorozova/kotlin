7acc61a44041773eb5bee308c1abbb103468bab1
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-05 22:05:32 +0200

    Cambios en Profile añadidos. Añadidos products.json y profiles.json

--- untitled/src/main/kotlin/profile/Profile.kt
+++ untitled/src/main/kotlin/profile/Profile.kt
@@ -4,14 +4,9 @@ import kotlinx.serialization.json.Json
 import java.io.File
 
 fun main() {
-    val file = File("profiles.json")
-    val profiles = loadProfiles(file)
+    val profiles = ProfilesRepository.profiles
     for (person in profiles){
         println(person)
     }
 }
 
-fun loadProfiles(file: File): List<Person>{
-    val content = file.readText().trim()
-    return Json.decodeFromString(content)
-}
