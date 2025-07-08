27501d4b14615897c88e81810cdaec92b94a18bf
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-08 07:21:54 +0200

    Cambios en Store añadidos(lambda expression)

--- untitled/src/main/kotlin/products/Store.kt
+++ untitled/src/main/kotlin/products/Store.kt
@@ -1,10 +1,15 @@
 package org.example.products
 
+
 import products.ProductCard
 
 fun main() {
 
     val products = ProductsRepository.products
+    var filtered = filter(products) { it.productPrice > 500 }
+    filtered = filter(filtered) {it.productCategory == ProductCategory.SPORTS }
+    filtered = filter(filtered) {it.productRating > 4 }
+    /*
     var filtered = filter(products, object : Condition{
         override fun isSuitable(productCard: ProductCard): Boolean {
             return productCard.productPrice > 500
@@ -21,6 +26,8 @@ fun main() {
         }
     })
 
+     */
+
     for (productCard in filtered){
         println(productCard)
     }
@@ -29,6 +36,16 @@ fun main() {
 //product sport
 //product high ranking
 
+fun filter(products: List<ProductCard>, isSuitable: (ProductCard) -> Boolean): List<ProductCard> {
+    val result = mutableListOf<ProductCard>()
+    for (productCard in products){
+        if (isSuitable(productCard)){
+            result.add(productCard)
+        }
+    }
+    return result
+}
+/*
 fun filter(products: List<ProductCard>, condition: Condition): List<ProductCard> {
     val result = mutableListOf<ProductCard>()
     for (productCard in products) {
@@ -38,3 +55,5 @@ fun filter(products: List<ProductCard>, condition: Condition): List<ProductCard>
     }
     return result
 }
+
+ */
