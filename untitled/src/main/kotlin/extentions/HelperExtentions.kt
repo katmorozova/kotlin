27d0cd8cfd18be8f2593d234453d813f53700e9e
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-09 16:41:09 +0200

    Cambios en HelperExtentions añadidos(List -> Iterable)

--- untitled/src/main/kotlin/extentions/HelperExtentions.kt
+++ untitled/src/main/kotlin/extentions/HelperExtentions.kt
@@ -1,6 +1,6 @@
 package org.example.extentions
 
-fun <R, T> List<T>.transform(operation: (T) -> R): List<R>{
+fun <R, T> Iterable<T>.transform(operation: (T) -> R): List<R>{
     val result = mutableListOf<R>()
     for (person in this){
         result.add(operation(person))
