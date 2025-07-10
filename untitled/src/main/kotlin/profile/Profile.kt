2abd3e589b50b976d2c2312d7512b371f1c813d0
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-10 08:10:26 +0200

    Cambios en Profile añadidos(forEach{})

--- untitled/src/main/kotlin/profile/Profile.kt
+++ untitled/src/main/kotlin/profile/Profile.kt
@@ -4,18 +4,15 @@ import org.example.extentions.transform
 
 
 fun main() {
-    val profiles = ProfilesRepository.profiles
+    ProfilesRepository.profiles
         .filter { it.age > 25 }
         .filter { it.gender == Gender.MALE }
         .filter { it.firstName.startsWith("A") }
         .filter { it.age < 30 }
         .toSet()
         .map { it.copy(age = it.age + 1) }
-        .map { it.firstName }
+        .forEach { println(it)}
 
-    for (person in profiles){
-        println(person)
-    }
 }
 
 
