c347a195248846790e85f9f0413e80d1f9909098
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-15 23:11:14 +0200

    Añadido metodo showDescription() en Dictionary

--- untitled/src/main/kotlin/dictionary/Dictionary.kt
+++ untitled/src/main/kotlin/dictionary/Dictionary.kt
@@ -9,8 +9,15 @@ fun main() {
     val file = File("dictionary.json")
     val content = file.readText().trim()
     val dictionary = Json.decodeFromString<List<Entry>>(content)
-    for (entry in dictionary){
-        println(entry)
-    }
+    showDescription(dictionary)
 
+}
+
+fun showDescription(dictionary: List<Entry>) {
+    while (true){
+        println("Enter word or 0 to exit: ")
+        val input = readln()
+        if (input == "0") break
+        dictionary.find { it.value == input }?.let { println(it.description)}?: println("Not found")
+    }
 }
