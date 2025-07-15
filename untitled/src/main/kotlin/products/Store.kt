3b1e693261221d2eca6e4339fd67a91101ad8583
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-15 22:32:53 +0200

    Cambios en Store añadidos(also{})

--- untitled/src/main/kotlin/products/Store.kt
+++ untitled/src/main/kotlin/products/Store.kt
@@ -1,13 +1,20 @@
 package org.example.products
 
+import org.example.extentions.myAlso
 import org.example.extentions.transform
 import products.ProductCard
 
 fun main() {
-    ProductsRepository.products
-        .filter { it.productCategory == ProductCategory.CLOTHING }
-        .map { it.copy(productPrice = it.productPrice * 2) }
-        .map { "${it.id} - ${it.productName} - ${it.productPrice}" }
+    ProductsRepository.products.myAlso {
+        println("Filter by category CLOTHING")
+    }
+        .filter { it.productCategory == ProductCategory.CLOTHING }.myAlso {
+            println("Increase price")
+        }.map { it.copy(productPrice = it.productPrice * 2) }.myAlso {
+            println("Convert to String")
+        }.map { "${it.id} - ${it.productName} - ${it.productPrice}" }.myAlso {
+            println("Print info")
+        }
         .forEach { println(it) }
 }
 
