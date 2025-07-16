06c4fe16c496f93149c5c2489733d75ace55a0c9
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-16 15:56:52 +0200

    Cambios en Test añadidos (Map collections)

--- untitled/src/main/kotlin/test/Test.kt
+++ untitled/src/main/kotlin/test/Test.kt
@@ -1,107 +1,28 @@
-package org.example.test
-
-import kotlinx.serialization.json.Json
-import java.io.File
-
-
-//val numbers = mutableListOf<Int>()
-
-var age: Int? = 20
+import org.example.profile.Condition
+import kotlin.collections.set
 
 fun main() {
+    val contacts = mutableMapOf(
+        "Hermes" to "699234234",
+        "Gala" to "634234543",
+        "Hermes" to "602385121"
+    )
 
-    val result = age?.let{
-        if (it >= 18){
-            "You are an adult"
-        }else{
-            "You will be an adult in ${18 - it} years"
-        }
-    }
-        result?.let{
-            println(it)
-        }
-    }
-
-
+    println("Enter name or 0: ")
+    val name = readln()
+    println("Enter phone number: ")
+    val phoneNumber = readln()
+    contacts[name] = phoneNumber
+    showContact(contacts)
 
 
 
-    //println(sum(5, 10))
-   // val file = File("test.txt")
-   // val number = file.readText().trim().toInt()
-    //println(sum(5, number))
-    /*
-    val file = File("items.json")
-    //writeToFile(file)
-
-    val items = readFromFile(file)
-    for(item in items){
-        println(item)
-    }
-     */
-
-
-/*
-fun readFromFile(file: File): List<Item> {
-    //val items = mutableListOf<Item>()
-    val content = file.readText().trim()
-    //val itemsAsString = content.split("\n")
-    return Json.decodeFromString<List<Item>>(content)
-   /*
-    for (itemAsString in itemsAsString){
-        val properties = itemAsString.split("%")
-        val id = properties[0].toInt()
-        val name = properties[1]
-        val item = Item(id, name)
-        items.add(item)
-    }
-
-
-    //return items
 }
-*/
-
-/*
-fun writeToFile(file: File){
-    val items = mutableListOf<Item>()
+fun showContact(contacts: Map<String, String>){
     while (true){
-        println("Enter id or 0 to exit: ")
-        val id = readln().toInt()
-        if (id == 0) break
-        println("Enter name: ")
+        println("Enter name or 0 to exit: ")
         val name = readln()
-        val item = Item(id, name)
-        items.add(item)
+        if(name == "0")break
+        println(contacts[name] ?: "Not found")
     }
-
-    //for (item in items){
-        //println(item)
-        //file.appendText("${item.id}%${item.name}\n")
-
-    //}
-    //val itemsAsString = Json.encodeToString(items)
-   // file.writeText(itemsAsString)
-//}*/
-/*
-fun sum(a:Int): Int {
-    val file = File("test.txt")
-    val number = file.readText().trim().toInt()
-    return a + number
 }
-
- */
-
-/*
-fun sum(a:Int, b: Int): Int {
-    return a + b
-}
-
- */
-/*
-fun sum(a:Int, b: Int): Int {
-    val result = a + b
-    numbers.add(result)
-    return result
-}
-
- */
